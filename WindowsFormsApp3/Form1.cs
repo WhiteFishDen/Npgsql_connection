29b78b86332b3906da8410b3809d4be878b49732
WhiteFishDen <muslinazavtrak@gmail.com>
2023-12-26 21:21:55 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string connString = "Host=localhost;Username=postgres;Password=123;Database=testDB";
+            using (NpgsqlConnection connection = new NpgsqlConnection(connString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show( "Connection is real!");
+                    NpgsqlCommand npgsqlCommand = connection.CreateCommand();
+                    npgsqlCommand.CommandText = "select * from public.employees";
+                    NpgsqlDataReader reader =  npgsqlCommand.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dataSet1.Tables.Add(dt);
+                    dataGridView1.AutoGenerateColumns = true;
+                    dataGridView1.DataSource = dataSet1.Tables[0];
+                    dataGridView1.Refresh();
+                    npgsqlCommand.Cancel();
+                    reader.Close();
+
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
