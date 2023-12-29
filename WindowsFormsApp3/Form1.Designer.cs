03b694c04c94a079879444f3d19ea70b6156e935
WhiteFishDen <muslinazavtrak@gmail.com>
2023-12-29 20:05:06 +0300

    comboBox_save_query

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -30,10 +30,12 @@
         {
             this.dataSet1 = new System.Data.DataSet();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-        
+            this.button_go_query = new System.Windows.Forms.Button();
+            this.label_query = new System.Windows.Forms.Label();
+            this.textBox_query = new System.Windows.Forms.TextBox();
+            this.comboBox_save_query = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
- 
             this.SuspendLayout();
             // 
             // dataSet1
@@ -43,28 +45,68 @@
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(726, 419);
+            this.dataGridView1.Size = new System.Drawing.Size(800, 237);
             this.dataGridView1.TabIndex = 0;
             // 
-            
+            // button_go_query
+            // 
+            this.button_go_query.Location = new System.Drawing.Point(210, 333);
+            this.button_go_query.Name = "button_go_query";
+            this.button_go_query.Size = new System.Drawing.Size(75, 23);
+            this.button_go_query.TabIndex = 2;
+            this.button_go_query.Text = "OK";
+            this.button_go_query.UseVisualStyleBackColor = true;
+            this.button_go_query.Click += new System.EventHandler(this.button_go_query_Click);
+            // 
+            // label_query
+            // 
+            this.label_query.AutoSize = true;
+            this.label_query.Location = new System.Drawing.Point(51, 285);
+            this.label_query.Name = "label_query";
+            this.label_query.Size = new System.Drawing.Size(38, 13);
+            this.label_query.TabIndex = 3;
+            this.label_query.Text = "Query:";
+            // 
+            // textBox_query
+            // 
+            this.textBox_query.Location = new System.Drawing.Point(104, 286);
+            this.textBox_query.Name = "textBox_query";
+            this.textBox_query.Size = new System.Drawing.Size(302, 20);
+            this.textBox_query.TabIndex = 4;
+            // 
+            // comboBox_save_query
+            // 
+            this.comboBox_save_query.FormattingEnabled = true;
+            this.comboBox_save_query.Location = new System.Drawing.Point(447, 285);
+            this.comboBox_save_query.Name = "comboBox_save_query";
+            this.comboBox_save_query.Size = new System.Drawing.Size(341, 21);
+            this.comboBox_save_query.TabIndex = 5;
+            this.comboBox_save_query.SelectedIndexChanged += new System.EventHandler(this.comboBox_save_query_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-          
+            this.Controls.Add(this.comboBox_save_query);
+            this.Controls.Add(this.textBox_query);
+            this.Controls.Add(this.label_query);
+            this.Controls.Add(this.button_go_query);
             this.Controls.Add(this.dataGridView1);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-         
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,7 +114,10 @@
 
         private System.Data.DataSet dataSet1;
         private System.Windows.Forms.DataGridView dataGridView1;
-     
+        private System.Windows.Forms.Button button_go_query;
+        private System.Windows.Forms.Label label_query;
+        private System.Windows.Forms.TextBox textBox_query;
+        private System.Windows.Forms.ComboBox comboBox_save_query;
     }
 }
 
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,29 +15,26 @@ namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
-            string connString = "Host=localhost;Username=postgres;Password=123;Database=testDB";
+            string connString = "Host=localhost;Username=postgres;Password=123;Database=postgres";
             using (NpgsqlConnection connection = new NpgsqlConnection(connString))
             {
                 try
                 {
                     connection.Open();
-                    MessageBox.Show( "Connection is real!");
                     NpgsqlCommand npgsqlCommand = connection.CreateCommand();
-                    npgsqlCommand.CommandText = "select * from public.employees";
+                    npgsqlCommand.CommandText = "select * from public.teachers";
                     NpgsqlDataReader reader =  npgsqlCommand.ExecuteReader();
-
                     DataTable dt = new DataTable();
                     dt.Load(reader);
-                    dataSet1.Tables.Add(dt);
+                    dataSet1.Tables.Add(dt); 
                     dataGridView1.AutoGenerateColumns = true;
                     dataGridView1.DataSource = dataSet1.Tables[0];
-                    dataGridView1.Refresh();
                     npgsqlCommand.Cancel();
                     reader.Close();
-
                     connection.Close();
                 }
                 catch (Exception ex)
@@ -49,6 +47,42 @@ namespace WindowsFormsApp3
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button_go_query_Click(object sender, EventArgs e)
+        {
+            string connString = "Host=localhost;Username=postgres;Password=123;Database=postgres";
+            string command = textBox_query.Text;
+            using (DbDataAdapter dbDataAdapter = new NpgsqlDataAdapter(command, connString))
+            {
+                try
+                {
+                    dbDataAdapter.Fill(dataSet1.Tables.Add());
+                    dataGridView1.DataSource = dataSet1.Tables[dataSet1.Tables.Count - 1];
+                    if (Check_history())
+                        comboBox_save_query.Items.Add(textBox_query.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!");
+                }
+            }
+        }
+        private bool Check_history()
+        {
+            for (int i = 0; i < comboBox_save_query.Items.Count; i++)
+            {
+                if (comboBox_save_query.Items.Count == 0)
+                    return true;
+                if (textBox_query.Text == comboBox_save_query.Items[i].ToString())
+                    return false; 
+            }
+            return true;
+        }
+        private void comboBox_save_query_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox_query.Text = comboBox_save_query.SelectedItem.ToString();
+            button_go_query_Click(sender, e);
         }
     }
 }
